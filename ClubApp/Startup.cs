efee5130a27829eb8654cb66b429c56f2a8a71ae
Chanuka Fernando <89817412+nukeboii@users.noreply.github.com>
2021-09-30 23:05:22 +1300

    removed unnecessary check

--- ClubApp/Startup.cs
+++ ClubApp/Startup.cs
@@ -46,7 +46,7 @@ namespace ClubApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            _userContext.Database.EnsureDeleted();
+            //_userContext.Database.EnsureDeleted();
             //Create the database if not exist
             _userContext.Database.EnsureCreated();
             app.UseHttpsRedirection();
